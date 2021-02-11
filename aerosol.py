17f2f15f680a91efca7093066e1dc1af11985a6a
alexpetercharles <hi@alexpetercharles.ch>
2021-02-11 12:00:58 +0100

    initial loss and optimizer

--- aerosol.py
+++ aerosol.py
@@ -80,3 +80,19 @@ def make_discriminator_model():
   model.add(Dense(1))
 
   return model
+
+# loss functions
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generator_loss(fake_output):
+  return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def discriminator_loss(real_output, fake_output):
+  real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+  fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+  total_loss = real_loss + fake_loss
+  return total_loss
+
+# optimizers
+  generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+  discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
