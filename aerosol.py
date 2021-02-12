f33463bccebbf7396b830af798218b5536d29e9d
alexpetercharles <hi@alexpetercharles.ch>
2021-02-12 19:30:39 +0100

    string quotes

--- aerosol.py
+++ aerosol.py
@@ -1,24 +1,11 @@
-# lexiboi 10.02.2020
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
-
-import time
-
 from pathlib import Path
 from tensorflow import keras
 
-from IPython import display
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, Dense, Reshape, Conv2DTranspose, Conv2D
-from tensorflow.keras.layers import BatchNormalization, LeakyReLU, Dropout, Flatten, ZeroPadding2D
-from tensorflow.python.framework import dtypes
+import models.discriminator
 
 data_dir = Path('./data/')
 
-# training data is scanned posters
+# poster data
 poster_height = 100 # px
 poster_width = 70
 poster_channels = 3
@@ -27,13 +14,13 @@ poster_channels = 3
 batch_size = 20
 
 #split dataset for training and validation
-validation_split = 0.01
+validation_split = 0.1
 
 # datasets
 train_ds = keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split = validation_split,
-  subset="training",
+  subset='training',
   seed = 123,
   image_size = (poster_height, poster_width),
   batch_size = batch_size)
@@ -41,10 +28,7 @@ train_ds = keras.preprocessing.image_dataset_from_directory(
 validate_ds = keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split = validation_split,
-  subset="validation",
+  subset='validation',
   seed = 123,
   image_size = (poster_height, poster_width),
   batch_size = batch_size)
-
-# model input noise
-input_shape = 100
