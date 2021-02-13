a385538834df206f363abadfbbed3df2ee9abb14
alexpetercharles <hi@alexpetercharles.ch>
2021-02-13 12:40:17 +0100

    image normalization funcctions

--- utils/image.py
+++ utils/image.py
@@ -8,9 +8,13 @@ def plot_posters(posters):
       # turn off axis
       pyplot.axis('off')
       # plot raw pixel data
-      pyplot.imshow(posters[i].numpy().astype('uint8'))
+      pyplot.imshow(posters[i].astype('float32'))
   pyplot.show()
 
 # normalize 0 : 255 to -1 : 1
-def normalize(dataset):
+def normalize_for_model(dataset):
   return (dataset - 127.5) / 127.5
+
+# normalize -1 : 1 to 0 : 1
+def normalize_to_float(dataset):
+  return (dataset + 1) / 2
