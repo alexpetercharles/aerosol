f9772971eabab04dce7ff193b9e5ee27503bae4f
alexpetercharles <hi@alexpetercharles.ch>
2021-02-13 15:20:05 +0100

    save posters util

--- utils/image.py
+++ utils/image.py
@@ -11,6 +11,20 @@ def plot_posters(posters):
       pyplot.imshow(posters[i].astype('float32'))
   pyplot.show()
 
+
+def save_posters(posters, epoch, n = 5):
+  posters = normalize_to_float(posters)
+  for i in range(5*5):
+    # define subplot
+    pyplot.subplot(n, n, 1 + i)
+    # turn off axis
+    pyplot.axis('off')
+    # plot raw pixel data
+    pyplot.imshow(posters[i].astype('float32'))
+  filename = 'posters/generated/generated_posters_e%03d.png' % (epoch+1)
+  pyplot.savefig(filename)
+  pyplot.close()
+
 # normalize 0 : 255 to -1 : 1
 def normalize_for_model(dataset):
   return (dataset - 127.5) / 127.5
