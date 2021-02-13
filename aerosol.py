71b9f19e9160bda837a19b7393d9a988b9d3ad37
alexpetercharles <hi@alexpetercharles.ch>
2021-02-13 12:59:31 +0100

    gan model definition

--- aerosol.py
+++ aerosol.py
@@ -33,15 +33,7 @@ validate_ds = keras.preprocessing.image_dataset_from_directory(
   image_size = (poster_height, poster_width),
   batch_size = batch_size)
 
-# from models import discriminator
-# discriminator_model = discriminator.define_model()
-# discriminator_model.summary()
-
-# from utils.image import normalize
-# for batch in train_ds:
-#  posters, _ = batch
-#  print(normalize(posters))
-
+# input dimensions
 latent_dim = 100
 
 from numpy.random import randn
@@ -52,6 +44,40 @@ def generate_latent_points(latent_dim, n_samples):
   # reshape for model input
   rand_input = rand_input.reshape(n_samples, latent_dim)
   return rand_input
+
+loss = 0.0002
+momentum = 0.5
+
+def define_gan():
+  gan = keras.models.Sequential()
+
+  from models import generator, discriminator
+
+  # add generator model
+  gan.add(generator.define_model(latent_dim))
+
+  discriminator_model = discriminator.define_model()
+  # discriminator untrainable
+  discriminator_model.trainable = False
+
+  # add discriminator model
+  gan.add(discriminator_model)
+
+  # optimizer
+  optimizer = keras.optimizers.Adam(loss, beta_1 = momentum)
+  # compile
+  gan.compile(loss = 'binary_crossentropy', optimizer = optimizer)
+
+  return gan
+
+
+gan = define_gan()
+gan.summary()
+
+
+  
+  
+  
 
 # plot the posters to verify the posters
 # from models import generator
