74bd4a627fddde49c87eb8d633750fdd2bf69eb4
alexpetercharles <hi@alexpetercharles.ch>
2021-02-13 11:57:44 +0100

    generate random input

--- aerosol.py
+++ aerosol.py
@@ -32,3 +32,27 @@ validate_ds = keras.preprocessing.image_dataset_from_directory(
   seed = 123,
   image_size = (poster_height, poster_width),
   batch_size = batch_size)
+
+# plot the posters to verify the posters
+# from utils.image import plot_posters
+# for batch in train_ds:
+#  posters, _ = batch
+#  plot_posters(posters)
+
+# from models import discriminator
+# discriminator_model = discriminator.define_model()
+# discriminator_model.summary()
+
+# from utils.image import normalize
+# for batch in train_ds:
+#  posters, _ = batch
+#  print(normalize(posters))
+
+from numpy.random import randn
+# generate random input for generator
+def generate_latent_points(latent_dim, n_samples):
+  # generate points in the latent space
+  rand_input = randn(latent_dim * n_samples)
+  # reshape for model input
+  rand_input = input.reshape(n_samples, latent_dim)
+  return rand_input
