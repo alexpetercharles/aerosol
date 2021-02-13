ec2601ae90d67b2e840b6bfde050e8646560fc99
alexpetercharles <hi@alexpetercharles.ch>
2021-02-13 11:52:25 +0100

    generator model

---
+++ models/generator.py
@@ -0,0 +1,49 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.keras.backend import dropout
+
+# poster data
+# x = 128 px
+# y = 96
+# z = 3 (rgb)
+# == (100, 70, 3)
+
+def define_model(latent_dim):
+
+  alpha = 0.2
+  foundation = 256 * 3 * 4
+
+  # sequential model means stacked layers
+  model = keras.models.Sequential()
+
+  model.add(layers.Dense(foundation, input_dim = latent_dim))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  model.add(layers.Reshape((4, 3, 256)))
+  # (4, 3, 256)
+
+  model.add(layers.Conv2DTranspose(256, (4, 4), strides = (2, 2), padding = 'same'))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  # (8, 6, 256)
+
+  model.add(layers.Conv2DTranspose(128, (4, 4), strides = (2, 2), padding = 'same'))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  # (16, 12, 128)
+
+  model.add(layers.Conv2DTranspose(128, (4, 4), strides = (2, 2), padding = 'same'))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  # (32, 24, 128)
+
+  model.add(layers.Conv2DTranspose(128, (4, 4), strides = (2, 2), padding = 'same'))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  # (64, 48, 128)
+
+  model.add(layers.Conv2DTranspose(128, (4, 4), strides = (2, 2), padding = 'same'))
+  model.add(layers.LeakyReLU(alpha = alpha))
+  # (128, 96, 128)
+
+  model.add(layers.Conv2D(3, (3, 3), activation= 'tanh', padding = 'same'))
+
+  return model
+
+# print model summary
+define_model(100).summary()
